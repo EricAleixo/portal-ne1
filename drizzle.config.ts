879da6937865bfe088f38f65bc03d55344740072
ericAleixo <ericaleixo66@gmail.com>
2026-02-09 08:45:27 -0300

    Update docker-compose

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
+const isProd = process.env.NODE_ENV === 'production'
+
+
 export default defineConfig({
   schema: "./app/_db/schema",
   out: "./app/_db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: isProd ? { rejectUnauthorized: false } : false,
   },
 });
