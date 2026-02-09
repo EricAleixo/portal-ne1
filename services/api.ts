af71e576c47f85aa12610c2a9456b607bc791c09
ericAleixo <ericaleixo66@gmail.com>
2026-02-09 10:34:03 -0300

    ADD apiurl

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+console.log('API_URL:', process.env.API_URL); // adicione isso!
+console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  baseURL: process.env.API_URL || 'http://52.23.184.237:3000', // fallback
   headers: {
     'Content-Type': 'application/json',
   },
