2e18773add1b2d0b90f9ebec51b311c88440320c
ericAleixo <ericaleixo66@gmail.com>
2026-02-09 11:06:17 -0300

    ADD apiurl

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://52.23.184.237:3000', // fallback
+  baseURL: 'http://52.23.184.237:3000',
   headers: {
     'Content-Type': 'application/json',
   },
