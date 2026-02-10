899fa8cb2374ab9cf967426b80c3fbce5edd5229
ericAleixo <ericaleixo66@gmail.com>
2026-02-09 21:20:41 -0300

    ADD: post delete

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import "dotenv/config";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://3.229.142.168:3000/api",
   headers: {
     'Content-Type': 'application/json',
   },
