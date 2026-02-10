c1987a18e50fa2b2499174e546baae6d7e6ac296
ericAleixo <ericaleixo66@gmail.com>
2026-02-10 10:48:34 -0300

    api url update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
-const isProd = process.env.NODE_ENV === 'production'
-
 
 export default defineConfig({
   schema: "./app/_db/schema",
   out: "./app/_db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
-    ssl: isProd ? { rejectUnauthorized: false } : false,
+    url: process.env.DATABASE_URL!
   },
 });
